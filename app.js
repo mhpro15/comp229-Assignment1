bc1190c20ebda92cdecb4388e3325c45fb378f38
hung <nguyenmanhhungpro15@gmail.com>
2023-05-15 17:22:18 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
