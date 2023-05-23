c47fc7e290542040df483e3929f25cc1517f386f
hung <nguyenmanhhungpro15@gmail.com>
2023-05-22 23:18:55 -0400

    update

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("content");
+  res.send("Coming Soon!");
 });
 
 module.exports = router;
