94d0d6ef2a762631cf23d147810bd8f4a040a7ff
hung <nguyenmanhhungpro15@gmail.com>
2023-06-08 21:02:49 -0400

    update

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -3,13 +3,12 @@ const passport = require("passport");
 const User = require('../modules/userModel');
 module.exports.displayLoginPage = async (req, res, next) => {
     try {
-         let foundUser = await User.findOne({ "username": "admin" }) 
-    console.log(foundUser);
-    if (foundUser == null) {
-            User.register(new User({ username: "admin" }), "password", (err, user) => {
+        let foundUser = await User.findOne({ "username": "admin" }) 
+        console.log(foundUser);
+        if (foundUser == null) {
+                User.register(new User({ username: "admin" }), "password", (err, user) => {
                 console.log("User registered");
             });
-        
     }
     } catch (error) {
         console.error(error);
